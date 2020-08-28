72ad33e80c6941c5ac4b80747d077320e6487b35
Vivid Green <zacharycreek@gmail.com>
2020-08-28 04:07:41 -0600

    added js functionality to get weather data and display to page.

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
-const apiKey = "6ae39a495699948a3a6ccf26e9ec4520";
-const url = "https://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey;
 let ukCloseBtn = $(".uk-offcanvas-close");
 let form = $("form");
-let city = $(".city");
+const apiKey = "6ae39a495699948a3a6ccf26e9ec4520"
 
-form.submit(function(event) {
-    event.preventDefault();
-    ukCloseBtn.click();
-    city = city.val();
-    console.log(url + "&q=" + city);
-    $.ajax({
-        url: url + "&q=" + city,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    }).catch(function(error) {
-        console.log(error);
+$(window).on("load", getWeather);
+
+function buildUrl(callType,params) {
+    let urlBase = "https://api.openweathermap.org/data/2.5/";
+    let weatherUrl = urlBase + callType + "?" + $.param(params);
+    return weatherUrl;
+}
+
+function setForecast(forecastData) {
+    console.log(forecastData);
+    console.log(forecastData.current);
+    let date = moment.unix(forecastData.current.dt).format("LL");
+    let iconImg = $("<img class='current-weather-icon' width='60' height='60'>");
+    let icon = forecastData.current.weather[0].icon;
+    iconImg.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+    let title = $(".current-title-dt h3");
+    title.text($("#city").val());
+    title.append(iconImg);
+    $(".current-title").text(date);
+    let currentStats = {
+        "Temperature": (Math.floor((forecastData.current.temp - 273.15) * 9/5 + 32)) + "°F",
+        "Humidity": forecastData.current.humidity + "%",
+        "Wind Speed": forecastData.current.wind_speed + " MPH",
+        "UV Index": forecastData.current.uvi
+    };
+
+    $(".current-body").empty();
+    $.each(currentStats, function(index,value) {
+        $(".current-body").append($("<p>" + index + ": " + value + "</p>"));
     });
-});
+
+    let forecastCards = $(".forecast-title-dt");
+
+    $.each(forecastCards, function(index,value) {
+        console.log(index,value);
+    });
+  
+    $(".current-weather").css("display", "block");
+    $(".forecast").css("display", "block");
+}
+
+function getForecast(weatherData) {
+    let callType = "onecall"
+    let params = {lat: weatherData.coord.lat, lon: weatherData.coord.lon, appid: apiKey};
+    let weatherUrl = buildUrl(callType,params);
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+    }).then(setForecast);
+}
+
+function getWeather(event) {
+    event.preventDefault();
+    let callType = "weather"
+    let params = {q: $("#city").val(), appid: apiKey};
+    let weatherUrl = buildUrl(callType,params);
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+    }).then(getForecast);
+    ukCloseBtn.click()
+}
+
+form.submit(getWeather);
